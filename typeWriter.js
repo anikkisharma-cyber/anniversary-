a994232ef7f38e0c74e5ca0194a64cbf38a1d1bc
anikkisharma-cyber <a.nikkisharma@gmail.com>
2026-03-31 18:36:04 +0530

    Update typeWriter.js

--- typeWriter.js
+++ typeWriter.js
@@ -1,36 +1,25 @@
-
 let i = 0;
-let text1 = "Hey! Handsome.";
-let text2 = "Today is the Big Day."
+let text1 = "Hey Handsome ❤️";
+let text2 = "Today is our special day 💖";
 let speed = 100;
+let ok = 0;
 
 function typeWriter(text, para){
-	if(ok == 2){
-		clearInterval(typeInterval);
-	}
-	if(i < text.length){
-		document.getElementById(para).innerHTML += text.charAt(i);
-		i++;
-		speed = Math.random() * 50 + 100;
-	}
-	else{
-		if(ok == 0){
-			i = 0;
-		}
-		ok += 1;
-	}
+if(i < text.length){
+document.getElementById(para).innerHTML += text.charAt(i);
+i++;
+}
+else{
+i = 0;
+ok++;
+}
 }
 
-var typeInterval;
-
-//window.onload = function() {
-//	window.onload = function(){};
-   	typeInterval = setInterval(function(){
-		if(ok == 0){
-			typeWriter(text1, "txt1");
-		}
-		else if(ok == 1){
-			typeWriter(text2, "txt2");
-		}
-	}, 100);
-//};
+setInterval(function(){
+if(ok == 0){
+typeWriter(text1, "txt1");
+}
+else if(ok == 1){
+typeWriter(text2, "txt2");
+}
+}, speed);
